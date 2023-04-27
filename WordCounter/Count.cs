0ddd801b5aeb2e1ceb90cfa23e63a3263b1961b1
Niels Faurskov <jazerixthegreat@gmail.com>
2023-04-27 09:08:41 +0200

    Update Count.cs

--- WordCounter/Count.cs
+++ WordCounter/Count.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 namespace WordCounter;
 
 /// <summary>
-/// Wrapper class since ints are passed by reference. 
+/// Wrapper class since ints are not passed by reference. 
 /// </summary>
 [DebuggerDisplay("{_count}", Name = "LO")]
 public class Count
